2f4a5c5b94ea6c7ce2aea307d7c3c4e42c5ffb06
ilan7empest <ilank7@gmail.com>
2020-06-05 19:32:44 +0300

    added theme-picker component

--- src/app/components/theme-picker/theme-picker.component.ts
+++ src/app/components/theme-picker/theme-picker.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'theme-picker',
   templateUrl: './theme-picker.component.html',
   styleUrls: ['./theme-picker.component.scss']
 })
-export class ThemePickerComponent implements OnInit {
 
-  constructor() { }
+export class ThemePickerComponent {
 
-  ngOnInit(): void {
+  constructor(
+  ) { }
+
+  themeMenuOpen = false;
+  toggleMain() {
+    this.themeMenuOpen = !this.themeMenuOpen;
   }
+
+  toggleTheme(theme: any) {
+    this.themeMenuOpen = false;
+
+    localStorage.setItem('theme-color', theme);
+    document.body.setAttribute('data-theme', theme);
+  }
+
+  themeList: any[] = [
+    {
+      theme: "light"
+    },
+    {
+      theme: "pink"
+    },
+    {
+      theme: "dark"
+    }
+  ];
 
 }
