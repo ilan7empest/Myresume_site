9efbfc44d74d460060d7339e0b905f2b4385fada
ilan7empest <ilank7@gmail.com>
2020-06-05 19:34:20 +0300

    added language compomemt

--- src/app/components/select-language/select-language.component.ts
+++ src/app/components/select-language/select-language.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'select-language',
   templateUrl: './select-language.component.html',
   styleUrls: ['./select-language.component.scss']
 })
-export class SelectLanguageComponent implements OnInit {
+  
+export class SelectLanguageComponent {
+  constructor(public translate: TranslateService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  switchLang(lang: string) {
+    this.translate.use(lang);
   }
 
+  langDirection: string;
+  setDirection(lang) {
+    lang == "en" ? this.langDirection = "ltr" : this.langDirection = "rtl";
+    document.querySelector('html').setAttribute('dir', this.langDirection);
+    localStorage.setItem('dir', this.langDirection);
+  }
 }
