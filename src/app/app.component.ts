50f66312f8fe9341de2efad70c6decf2a1425b02
ilan7empest <ilank7@gmail.com>
2020-06-08 20:08:59 +0300

    removed old code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+  
 export class AppComponent implements OnInit {
   title = 'my-resume';
   
