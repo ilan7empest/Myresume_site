ac272bc5ab4395c1e9b69d61c01d5067e98cc0a6
ilan7empest <ilank7@gmail.com>
2020-06-13 03:40:28 +0300

    Auto-generated commit

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,11 +5,13 @@ window.onscroll = function() {
 function scrollFunction() {
   var header = document.getElementById("header");
   var main = document.getElementById("nav");
-  if (document.body.scrollTop > 130 || document.documentElement.scrollTop > 130) {
-    header.classList.add("scrolled");
-    main.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-    main.classList.remove("scrolled");
-  }
+    if (header != null && main != null) {
+        if (document.body.scrollTop > 130 || document.documentElement.scrollTop > 130) {
+            header.classList.add("scrolled");
+            main.classList.add("scrolled");
+        } else {
+            header.classList.remove("scrolled");
+            main.classList.remove("scrolled");
+        }
+    }
 }
