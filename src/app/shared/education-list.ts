dc23a4fc9da3ba507215d1c139e30f5e14678a79
ilan7empest <ilank7@gmail.com>
2020-06-15 04:14:16 +0300

    ui fix

--- src/app/shared/education-list.ts
+++ src/app/shared/education-list.ts
@@ -2,18 +2,11 @@ import { Education } from './education';
 
 export const EDUCATIONLIST: Education[] = [
     {
-        date: '2001-2003',
-        school: 'Kinneret Academic College',
-        diploma: 'Practical Engineer',
-        location: 'Jordan Valley',
-        description: 'Majored in Graphic Design, Programming and Building web sites using Video, Sound, Animation with main concentration in UX/UI'
-    },
-    {
-        date: '2014',
+        date: '2018',
         school: 'Matrix',
-        diploma: 'AngualrJS - Basics',
-        location: 'Hertzeliya',
-        description: 'Intoduction to AngularJS Basic. Overview on SPA, angular development enviroment and usage'
+        diploma: 'Agile Methodology',
+        location: 'Matrix Hertzeliya',
+        description: 'Iterative approach to project management and software development that helps teams deliver value to their customers faster and with fewer problems.'
     },
     {
         date: '2014',
@@ -23,10 +16,17 @@ export const EDUCATIONLIST: Education[] = [
         description: 'Developing managerial skills, Employee motivation, Interpersonal communication as a management tool, Meeting management, briefings and presentation'
     },
     {
-        date: '2018',
+        date: '2014',
         school: 'Matrix',
-        diploma: 'Agile Methodology',
+        diploma: 'AngualrJS - Basics',
         location: 'Matrix Hertzeliya',
-        description: 'Iterative approach to project management and software development that helps teams deliver value to their customers faster and with fewer problems.'
+        description: 'Intoduction to AngularJS Basic. Overview on SPA, angular development enviroment and usage'
+    },
+    {
+        date: '2001-2003',
+        school: 'Kinneret Academic College',
+        diploma: 'Practical Engineer',
+        location: 'Jordan Valley',
+        description: 'Majored in Graphic Design, Programming and Building web sites using Video, Sound, Animation with main concentration in UX/UI'
     }
 ]
