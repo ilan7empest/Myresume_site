a8e48e55e0e34a5b5b843eba2db00b1cea49957b
ilan7empest <ilank7@gmail.com>
2020-06-25 16:48:12 +0300

    moved scroll into components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class AppComponent implements OnInit {
     let scrollToTop = window.setInterval(() => {
       let pos = window.pageYOffset;
       if (pos > 0) {
-        window.scrollTo(0, pos - 20); // how far to scroll on each step
+        window.scrollTo(0, pos - 50); // how far to scroll on each step
       } else {
         window.clearInterval(scrollToTop);
       }
