ccec52f1ae6693509ac92fcd161f4d4b1efc6f9c
ilan7empest <ilan7empest@somemail.com>
2020-08-05 09:54:41 +0300

    added text changes

--- src/app/shared/education-list.ts
+++ src/app/shared/education-list.ts
@@ -1,32 +1,52 @@
 import { Education } from './education';
 
 export const EDUCATIONLIST: Education[] = [
-    {
-        date: '2018',
-        school: 'Matrix',
-        diploma: 'Agile Methodology',
-        location: 'Matrix Hertzeliya',
-        description: 'Iterative approach to project management and software development that helps teams deliver value to their customers faster and with fewer problems.'
-    },
-    {
-        date: '2014',
-        school: 'Matrix',
-        diploma: 'Executive Training Course',
-        location: 'John Bryce TLV',
-        description: 'Developing managerial skills, Employee motivation, Interpersonal communication as a management tool, Meeting management, briefings and presentation'
-    },
-    {
-        date: '2014',
-        school: 'Matrix',
-        diploma: 'AngualrJS - Basics',
-        location: 'Matrix Hertzeliya',
-        description: 'Intoduction to AngularJS Basic. Overview on SPA, angular development enviroment and usage'
-    },
-    {
-        date: '2001-2003',
-        school: 'Kinneret Academic College',
-        diploma: 'Practical Engineer',
-        location: 'Jordan Valley',
-        description: 'Majored in Graphic Design, Programming and Building web sites using Video, Sound, Animation with main concentration in UX/UI'
-    }
-]
+  {
+    date: '2020',
+    school: 'Udemy',
+    diploma: 'JS: Full-Stack from Scratch',
+    location: '',
+    description:
+      'Understand the JavaScript language, Node.js, MongoDB, The Web Browser and More To Create Meaningful Applications.',
+  },
+  {
+    date: '2020',
+    school: 'Coursera',
+    diploma: 'Angular Framework',
+    location: '',
+    description:
+      'Angular Framework, components, directives and services. Introduction to Observables, reactive programming and RxJS. Angular support for client-server communication through the HTTP client and the use of REST API on the server side',
+  },
+  {
+    date: '2018',
+    school: 'Matrix',
+    diploma: 'Agile Methodology',
+    location: 'Matrix Hertzeliya',
+    description:
+      'Iterative approach to project management and software development that helps teams deliver value to their customers faster and with fewer problems.',
+  },
+  {
+    date: '2014',
+    school: 'Matrix',
+    diploma: 'Executive Training Course',
+    location: 'John Bryce TLV',
+    description:
+      'Developing managerial skills, Employee motivation, Interpersonal communication as a management tool, Meeting management, briefings and presentation',
+  },
+  {
+    date: '2014',
+    school: 'Matrix',
+    diploma: 'AngualrJS - Basics',
+    location: 'Matrix Hertzeliya',
+    description:
+      'Intoduction to AngularJS Basic. Overview on SPA, angular development enviroment and usage',
+  },
+  {
+    date: '2001-2003',
+    school: 'Kinneret Academic College',
+    diploma: 'Practical Engineer',
+    location: 'Jordan Valley',
+    description:
+      'Graphic Design, UX/UI, Programming Languages, Marketing, Advertisement, Video, Photography, Flash ,Sound',
+  },
+];
--- src/app/shared/experience-list.ts
+++ src/app/shared/experience-list.ts
@@ -1,70 +1,83 @@
 import { Experience } from './experience';
 
 export const EXPERIENCELIST: Experience[] = [
-    {
-        date: '2014-2020',
-        company: 'Matrix - "Bank Leumi"',
-        jobTitle: 'UX/UI Team Leader',
-        description: [
-            {
-                description: 'Head of an Angular development team (Management)'
-            },
-            {
-                description: 'Building and managing a team of 6 programmers'
-            },
-            {
-                description: 'Overseeing development from product design concept to integration across multiple teams'
-            },
-            {
-                description: 'Responsible for the entire UI/UX Development, Guidelines, Standards and Solutions'
-            },
-            {
-                description: 'Design overview, directly working, consulting and guiding UX Studio teams'
-            },
-            {
-                description: 'Managing various design activities'
-            }
-        ]
-    },
-    {
-        date: '2013-2014',
-        company: 'Matrix - "Bank Leumi"',
-        jobTitle: 'Web Developer',
-        description: [
-            {
-                description: 'Building and Maintaining E-banking sites on various platforms (.NET & AngularJS).'
-            },
-            {
-                description: 'Integrating new features.'
-            },
-            {
-                description: 'Web Performance Optimization.'
-            }
-        ]
-    },
-    {
-        date: '2004-2013',
-        company: 'CoolVision Ltd.',
-        jobTitle: 'Web Developer',
-        description: [
-            {
-                description: 'Managed the development and execution of multi level web-sites in English that have large international recognition.'
-            },
-            {
-                description: 'Generating million of dollars in revenue.'
-            },
-            {
-                description: 'Oversee bi-monthly marketing campaigns in English for various affiliate sites.'
-            },
-            {
-                description: 'Building and integrating marketing promotional tools in various formats.'
-            },
-            {
-                description: 'Act as lead liaison between creative and technical team to ensure timelines are met.'
-            },
-            {
-                description: 'Day to day work in technical programs such as ASP and .net environment.'
-            }
-        ]
-    }
-]
+  {
+    date: '2014-2020',
+    company: 'Matrix - "Bank Leumi"',
+    jobTitle: 'Front End Team Leader',
+    description: [
+      {
+        description:
+          'Implemented new features in collaboration with various back-end teams',
+      },
+      {
+        description:
+          'Led design overviews, consulted and guided UX Studio teams from product conception to integration',
+      },
+      {
+        description:
+          'Built and managed a team of 6 Angular programmers and UI designers',
+      },
+      {
+        description:
+          'Led site restructure from the ground up using technologies such as AngularJS framework and Bootstrap library',
+      },
+      {
+        description:
+          'Initialized SASS and HTML consultation for back-end programmers, result: 40% increase in work productivity and decrease dependency in front - end team',
+      },
+    ],
+  },
+  {
+    date: '2013-2014',
+    company: 'Matrix - "Bank Leumi"',
+    jobTitle: 'Web Developer',
+    description: [
+      {
+        description:
+          'Built and Maintained E-banking sites on various platforms',
+      },
+      {
+        description: 'Added site accessibility according to WCAG',
+      },
+      {
+        description:
+          'Increased site performance by 12% using icon font and sprites. Reduced images requests by 70%',
+      },
+      {
+        description:
+          'Implemented a new responsive , mobile first approach, standardized UI/UX development guidelines and best practices',
+      },
+    ],
+  },
+  {
+    date: '2004-2013',
+    company: 'CoolVision Ltd.',
+    jobTitle: 'Web Developer',
+    description: [
+      {
+        description:
+          'Managed the development and execution of multi level web-sites in English that have large international recognition.',
+      },
+      {
+        description: 'Generating million of dollars in revenue.',
+      },
+      {
+        description:
+          'Oversee bi-monthly marketing campaigns in English for various affiliate sites.',
+      },
+      {
+        description:
+          'Building and integrating marketing promotional tools in various formats.',
+      },
+      {
+        description:
+          'Act as lead liaison between creative and technical team to ensure timelines are met.',
+      },
+      {
+        description:
+          'Built an affiliation end user system for promotional tools in various formats with development team collaboration.Result, 30% rise in               conversions',
+      },
+    ],
+  },
+];
